a1937707848d7adf0a2dd7db153e1ac1757d0616
oneway <trust.biz888@gmail.com>
2022-04-07 07:30:02 -0500

    fix warning of in search result component

--- reactComponents/src/searchbar/tokenSearch/SearchResult.tsx
+++ reactComponents/src/searchbar/tokenSearch/SearchResult.tsx
@@ -1,29 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import 'twin.macro';
-import {styled} from 'styled-components/macro'
+import 'styled-components/macro'
 
 import {RootState} from "../redux/store";
 import TokenPairDetail from './TokenPairDetail'
-
-const NilFoundContainer = styled.div`
-  width: 50%;
-  margin-left: 25%;
-  margin-right: 25%;
-  margin-top: -5px;
-  position: relative;
-  background-color: #1c646c;
-  z-index: 100;
-  color: rgba(0, 0, 0, 0.87);
-  height: 60px;
-  text-align: center;
-  color: white;
-  font-weight: bolder;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
+ 
 const SearchResult = (props) => {
   const {suggestions, searchText} = useSelector(
     (state:RootState) => state
@@ -32,13 +14,13 @@ const SearchResult = (props) => {
     .slice()
     .sort((pair1, pair2) => pair2.volumeUSD - pair1.volumeUSD);
 
-  if (props.loading) {
-    return <NilFoundContainer>Loading...</NilFoundContainer>;
-  }
-
-  if (!!searchText && !filteredSuggestions.length) {
-    return <NilFoundContainer>No pairs found...</NilFoundContainer>;
-  }
+    if (props.loading) {
+      return <div tw="relative flex bg-white justify-center items-center">Loading...</div>;
+    }
+  
+    if (!!searchText && !filteredSuggestions.length) {
+      return <div tw="relative flex bg-white justify-center items-center">No pairs found...</div>;
+    }  
 
   return (
     <div tw="h-60 overflow-y-auto pl-4 border-solid">    
