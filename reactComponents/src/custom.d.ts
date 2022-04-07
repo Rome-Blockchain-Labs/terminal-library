8cfb54ef587265b318f3fe2e9faae5caa9ed1e91
Mark Camball <mark.camball@gmail.com>
2022-04-06 20:44:08 -0400

    export accordian types

--- reactComponents/src/custom.d.ts
+++ reactComponents/src/custom.d.ts
@@ -2,12 +2,3 @@ declare module "*.svg" {
   const content: any;
   export default content;
 }
-
-declare module "react-accessible-accordion" {
-  export const Accordion:any
-  export const AccordionItem: any
-  export const AccordionItemHeading: any
-  export const AccordionItemButton: any
-  export const AccordionItemPanel: any
-
-}
