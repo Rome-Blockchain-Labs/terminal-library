c0b904a6cf778731f6aafa6e915126f5ba1b4a5c
Ian de Jesus <ian.dejesus.md@gmail.com>
2022-04-07 12:57:14 +0900

    fix: add fixes to pubslish to npm

--- reactComponents/README.md
+++ reactComponents/README.md
@@ -1,15 +1,22 @@
 ### npm install dependency in package.json
+
 ```
 
     "@rbl/terminal-library": "git+https://github.com/Rome-Blockchain-Labs/terminal-library.git#8dda04858acb1c45437325857eff56c60afca5ee"
 ```
 
 ### import and use the react react component
+
 import {HelloWorldComponent} from "@rbl/terminal-library/reactComponents/dist"
 
 ```
 <div>
-    <HelloWorldComponent/>    
+    <HelloWorldComponent/>
 </div>
 ```
 
+### Publishing Instructions
+
+1. Go to `react-virtuazlied/dist/es/WindowScroller/utils/onScroll.js`
+2. Delete the last line `import { bpfrpt_proptype_WindowScroller } from "../WindowScroller.js";`
+3. `npm run publish`
--- reactComponents/src/searchbar/index.tsx
+++ reactComponents/src/searchbar/index.tsx
@@ -1,18 +1,16 @@
-import React from "react"
-import './App.css';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import React from "react";
+import "./App.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import TokenSearch from "./tokenSearch";
 
 export function SearchBar() {
   return (
     <div className="App">
       <Provider store={store}>
-      <br/><br/><br/><br/><br/><br/>
-      <br/><br/>
-      <div style={{width:"500px", margin:"auto", border:"solid"}}>
-        <TokenSearch />
-      </div>
+        <div style={{ width: "500px", margin: "auto", border: "solid" }}>
+          <TokenSearch />
+        </div>
       </Provider>
     </div>
   );
