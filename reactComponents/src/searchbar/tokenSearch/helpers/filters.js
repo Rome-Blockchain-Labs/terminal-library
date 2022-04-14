03a1fad762c4ae8e25abf27d5e54b86350b50c27
CP2053 <guillaume@romeblockchain.com>
2022-04-13 20:58:49 -0400

    Added spread operator

--- reactComponents/src/searchbar/tokenSearch/helpers/filters.js
+++ reactComponents/src/searchbar/tokenSearch/helpers/filters.js
@@ -4,4 +4,4 @@ export const filterActiveAll = filteredData => !Object.values(filteredData).some
 
 export const filterActiveNames = filteredData => Object.entries(filteredData).filter(entry => entry[1]).map(entry => entry[0]);
 
-export const filterValidExchangeNames = networkNames => Array.from(new Set(networkExchangePairs.filter(network => filterActiveNames(networkNames).includes(network[0])).map(network => network[1])));
+export const filterValidExchangeNames = networkNames => [...new Set(networkExchangePairs.filter(network => filterActiveNames(networkNames).includes(network[0])).map(network => network[1]))];
