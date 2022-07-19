6754bb73bd64457dc71eccc204a22a0a532796c5
iandjx <iandjx@gmail.com>
2022-07-19 18:16:52 +0800

    fix: add prettier, remove double quotations, update packages

--- WalletContext/example/src/App.js
+++ WalletContext/example/src/App.js
@@ -1,22 +1,22 @@
-import './App.css'
-import { MetaMask } from '@web3-react/metamask'
-import { Network } from '@web3-react/network'
-import { WalletConnect } from '@web3-react/walletconnect'
+import "./App.css"
+import { MetaMask } from "@web3-react/metamask"
+import { Network } from "@web3-react/network"
+import { WalletConnect } from "@web3-react/walletconnect"
 
-import { SUPPORTED_WALLETS, useWeb3React , useWallets } from "@romeblockchain/wallet""
+import { SUPPORTED_WALLETS, useWeb3React, useWallets } from "@romeblockchain/wallet"
 
 function App() {
   const { connector, account } = useWeb3React()
   const { setSelectedWallet } = useWallets()
   return (
     <>
-      <div style={{ display: 'flex', width: '100%', padding: '2.5rem' }}>
+      <div style={{ display: "flex", width: "100%", padding: "2.5rem" }}>
         <div
           style={{
-            width: '50%',
-            border: '1px solid black',
-            marginRight: 'auto',
-            marginLeft: 'auto',
+            width: "50%",
+            border: "1px solid black",
+            marginRight: "auto",
+            marginLeft: "auto",
           }}
         >
           <div>Priority Wallet</div>
@@ -24,20 +24,29 @@ function App() {
           <div>Account: {account}</div>
         </div>
       </div>
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem', alignItems: 'center' }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "0.5rem",
+          alignItems: "center",
+        }}
+      >
         {Object.keys(SUPPORTED_WALLETS).map((key) => {
           const wallet = SUPPORTED_WALLETS[key]
           const activeWallet = getName(connector) === wallet.name
-          const activeColor = activeWallet ? 'lightblue' : 'gray'
+          const activeColor = activeWallet ? "lightblue" : "gray"
           return (
             <button
-              style={{ backgroundColor: activeColor, width: '40%', height: '50px' }}
+              style={{ backgroundColor: activeColor, width: "40%", height: "50px" }}
               onClick={() => {
                 setSelectedWallet(wallet.wallet)
                 wallet.connector.activate()
               }}
             >
-              {activeWallet ? `Connected to ${wallet.name}` : `Connect to ${wallet.name}`}
+              {activeWallet
+                ? `Connected to ${wallet.name}`
+                : `Connect to ${wallet.name}`}
             </button>
           )
         })}
@@ -49,8 +58,8 @@ function App() {
 export default App
 
 export function getName(connector) {
-  if (connector instanceof MetaMask) return 'MetaMask'
-  if (connector instanceof WalletConnect) return 'WalletConnect'
-  if (connector instanceof Network) return 'Network'
-  return 'Unknown'
+  if (connector instanceof MetaMask) return "MetaMask"
+  if (connector instanceof WalletConnect) return "WalletConnect"
+  if (connector instanceof Network) return "Network"
+  return "Unknown"
 }
