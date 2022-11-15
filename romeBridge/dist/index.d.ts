36046607927f49d76da9d75146448c1192a1c3fa
iandjx <iandjx@gmail.com>
2022-11-15 16:18:29 +0800

    delete publish script

--- romeBridge/dist/index.d.ts
+++ romeBridge/dist/index.d.ts
@@ -27,15 +27,6 @@ declare class WidgetUpdateButtonStatusEvent extends RomeEvent {
 declare class WidgetBridge {
     widgetId: any;
     init(): void;
-    /**
-     * There may be potential issues if the widget wants to use storage/cookies before it is initialized
-     * As it won't be on the namespace yet.
-     *
-     * A workaround for this would be for the widget to call this function directly with the prefix hardcoded
-     *
-     * A longer term solution to explore as a fix may be to pass the prefix to the widget via url params
-     * **/
-    namespaceCookiesStorage(prefix: string): void;
     emit(type: RomeEventType, payload: any): void;
     subscribe<T>(type: RomeEventType, handler: RomeEventHandler<T>): void;
 }
