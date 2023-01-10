0e33ebefda6803c996e09b80a63297e241946df2
Ian de Jesus <ian@romeblockchain.com>
2023-01-10 14:13:12 +0800

    simplify handleconnect logic, add loading var

--- WalletContext/src/index.ts
+++ WalletContext/src/index.ts
@@ -1,16 +1,5 @@
-import WalletProvider, { SUPPORTED_WALLETS } from './context/WalletProvider'
-import { useWeb3React } from '@web3-react/core'
-import { useWallets } from './hooks/useWallets'
-import { Wallet } from './types'
-import { getAddChainParametersfromNetworkName, getAddChainParameters, NetworkName } from './chains'
-
-export {
-  WalletProvider,
-  SUPPORTED_WALLETS,
-  useWeb3React,
-  Wallet,
-  useWallets,
-  getAddChainParametersfromNetworkName,
-  getAddChainParameters,
-  NetworkName,
-}
+export * from '@web3-react/core'
+export * from './hooks/useWallets'
+export * from './chains'
+export * from './connection/connectors'
+export * from './context/WalletProvider'
